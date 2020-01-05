9f89c7e93fa6752ca96d88f735d03adbd3cf6efd
JiYujun <812997220@qq.com>
2020-01-05 11:02:05 +0800

    jyj

--- src/main/java/com/briup/restaurant/bean/ex/EndWait.java
+++ src/main/java/com/briup/restaurant/bean/ex/EndWait.java
@@ -3,6 +3,15 @@ package com.briup.restaurant.bean.ex;
 public class EndWait {
     private int id;
     private String phoneNumber;
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 
     public int getId() {
         return id;
