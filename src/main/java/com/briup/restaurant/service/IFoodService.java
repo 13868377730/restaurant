8cf6f270497502a4b5dbbb23b6595426b7f8c2b6
lihj <3109295205@qq.com>
2020-01-05 10:56:55 +0800

    lhj

--- src/main/java/com/briup/restaurant/service/IFoodService.java
+++ src/main/java/com/briup/restaurant/service/IFoodService.java
@@ -1,6 +1,7 @@
 package com.briup.restaurant.service;
 
 import com.briup.restaurant.bean.Food;
+import com.briup.restaurant.bean.ex.FoodSales;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface IFoodService {
     List<Food> selectAll() throws  RuntimeException;
     List<Food> selectBy (String key1,String key2,String word) throws  RuntimeException;
      Food selectById(int id) throws  RuntimeException;
+     List<Food> selectByState() throws  RuntimeException;
+    List<FoodSales> selectSales(String date1, String date2);
 }
