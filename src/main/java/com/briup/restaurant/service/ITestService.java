bca642c377d29a3586f19a7f7b233f8fe37c9370
JiYujun <812997220@qq.com>
2020-01-06 10:15:05 +0800

    jyj

--- src/main/java/com/briup/restaurant/service/ITestService.java
+++
@@ -1,4 +0,0 @@
-package com.briup.restaurant.service;
-
-public interface ITestService {
-}
--- src/main/java/com/briup/restaurant/service/impl/TestServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.briup.restaurant.service.impl;
-
-import com.briup.restaurant.service.ITestService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class TestServiceImpl implements ITestService {
-}
